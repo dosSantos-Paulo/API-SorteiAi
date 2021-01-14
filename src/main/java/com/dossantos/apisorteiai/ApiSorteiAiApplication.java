760f2560fe93ad984c497348ee8c3b6ac6bfc1f3
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-14 13:00:19 -0300

    init project

---
+++ src/main/java/com/dossantos/apisorteiai/ApiSorteiAiApplication.java
@@ -0,0 +1,13 @@
+package com.dossantos.apisorteiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSorteiAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSorteiAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dossantos/apisorteiai/ApiSorteiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dossantos.apisorteiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSorteiAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
