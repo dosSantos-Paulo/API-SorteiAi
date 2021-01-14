b4cbb59c96c053c60969a4b5bd6af5c515089822
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-14 15:50:28 -0300

    implementando validação do método

--- src/main/java/com/dossantos/sorteiai/model/NumberGenerator.java
+++ src/main/java/com/dossantos/sorteiai/model/NumberGenerator.java
@@ -5,15 +5,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class NumberGenerator {
 
-    public String generator(Integer number) {
-        String luckyNumber = "";
+    public String generator(Integer number) throws Exception {
 
-        for (int i = 1; i <= number; i++) {
-            Integer randomNumber = Math.toIntExact(Math.round(Math.random() * 9));
-            luckyNumber = luckyNumber + randomNumber.toString();
+        if (number >= 4 && number <= 6) {
+            String luckyNumber = "";
+
+            for (int i = 1; i <= number; i++) {
+                Integer randomNumber = Math.toIntExact(Math.round(Math.random() * 9));
+                luckyNumber = luckyNumber + randomNumber.toString();
+            }
+
+
+            return luckyNumber;
+        } else {
+            throw new Exception("the number must be between 4 and 6");
         }
 
-
-        return luckyNumber;
     }
 }
